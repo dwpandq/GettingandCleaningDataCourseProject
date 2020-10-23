7b302cacd718d3fc3aea82e182f1dd6b4c7eedd7
Daniel Payne <dwpandq@gmail.com>
2020-10-23 16:47:32 -0400

    Attempt to add first commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,12 @@
 # Loading appropriate library for project
 library(dplyr)
 
+# Clean up environment
+rm(list = ls())
+
+#################
+# Download Data #
+#################
 
 # Variables for download to reference
 filename <- "dataset.zip"
@@ -15,19 +21,48 @@ if (!file.exists(filename)){
 if (!file.exists("UCI HAR Dataset")) { 
     unzip(filename) 
 }
+########################################################################
+# Step 1. Merges the training and the test sets to create one data set.#
+########################################################################
 
-# 1. Merges the training and the test sets to create one data set.
+# In order to prepare to merge data files - Create dataframes for the
+# appropriate txt files
+features <- read.table("UCI HAR Dataset/features.txt", 
+                       col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", 
+                         col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", 
+                           col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", 
+                     col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", 
+                     col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", 
+                            col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", 
+                      col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", 
+                      col.names = "code")
+###########################################################################
+# 2. Extracts only the measurements on the mean and standard deviation for# 
+# each measurement.                                                       #
+###########################################################################
 
 
-# 2. Extracts only the measurements on the mean and standard deviation for 
-# each measurement.
+
+#############################################################################
+# 3. Uses descriptive activity names to name the activities in the data set #
+#############################################################################
 
 
-# 3. Uses descriptive activity names to name the activities in the data set
+#########################################################################
+# 4. Appropriately labels the data set with descriptive variable names. #
+#########################################################################
 
 
-# 4. Appropriately labels the data set with descriptive variable names.
 
+###############################################################################
+# 5. From the data set in step 4, creates a second, independent tidy data set #
+# with the average of each variable for each activity and each subject.       #
+###############################################################################
 
-# 5. From the data set in step 4, creates a second, independent tidy data set 
-# with the average of each variable for each activity and each subject.
